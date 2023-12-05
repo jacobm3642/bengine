d200b13eb6e9881a6d39a6f7be2bba0b33646374
Ben <bgo0026@arastudent.ac.nz>
2023-12-05 21:58:34 +1300

    build better

--- build.bat
+++ build.bat
@@ -3,5 +3,6 @@ set "folderPath=build"
 if not exist "%folderPath%" (
     mkdir "%folderPath%"
 )
-cl ./main.c /Fe:build/main.exe /link user32.lib
+set "compilerFlag=E:\glew-2.2.0\lib\Release\Win32\glew32.lib user32.lib gdi32.lib opengl32.lib"
+cl main.c /EHsc /I"E:\glew-2.2.0\include" /I"E:\glew-2.2.0\lib" /Fe:build/main.exe /link %compilerFlag%
 rm main.obj
